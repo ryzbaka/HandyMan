89defe9cff0598f057ebadd7e0b359d3d4751761
Hamza Ali Rizvi <hamzaali151214@hotmail.com>
2019-11-07 00:25:02 +0530

    added buttons to sign in and sign up form

---
+++ public/signin_logic.js
@@ -0,0 +1,4 @@
+const signInButton=document.querySelector('.sign-in-button')
+signInButton.addEventListener('click',()=>{
+    alert("Work in progress")
+})
---
+++ public/signup_logic.js
@@ -0,0 +1,4 @@
+const signUpButton=document.querySelector('.sign-up-button')
+signUpButton.addEventListener('click',()=>{
+    alert("Work in Progress")
+})
